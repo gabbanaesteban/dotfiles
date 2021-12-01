465e2cb8877f8f5ea275599b9596b3ab58cdee48
Esteban <gabbanaesteban@gmail.com>
2021-12-01 02:24:39 -0400

    wip

--- requirements.sh
+++ requirements.sh
@@ -32,35 +32,34 @@ function error() {
   echo -e "$COL_RED [error]$COL_RESET "$1
 }
 
+function reloadzsh() {
+  source zsh_files/.zshrc
+}
+
 bot "Hello! Let's get some stuff ready for your new computer"
 
 bot "Installing osx command line tools"
 xcode-select --install
 
-# create a symblink for all zsh files
-ln -sf ~/dotfiles/zsh_files/.* ~/
-
 # install brew
 running "Installing brew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+reloadzsh
 
 running "Updating to most recent brew version"
 brew doctor
 brew update
 ok
 
-running "Downloading oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ok
-
-bot "Setting zsh as the user shell"
-chsh -s /bin/zsh
-ok
+# running "Downloading oh-my-zsh"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# ok
 
 running "Downloading nvm"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh)"
 
-source zsh_files/.zshrc
+reloadzsh
 
 running "Installing the latest stable version of node"
 nvm install
@@ -68,8 +67,14 @@ nvm install
 running "Installing zx globally"
 npm i -g zx
 
-source zsh_files/.zshrc
-ok
+reloadzsh
 
 bot "Whooo, all set! "
 bot "Run zx ./setup.sh to setup your environment"
+
+# bot "Setting zsh as the user shell"
+# chsh -s /bin/zsh
+# ok
+
+# create a symblink for all zsh files
+ln -sf ~/dotfiles/zsh_files/.* ~/
