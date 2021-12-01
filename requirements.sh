2c98d94c4ca3cb798fdbf80523f5efbbec19b688
Esteban <gabbanaesteban@gmail.com>
2021-12-01 02:42:14 -0400

    wip

--- requirements.sh
+++ requirements.sh
@@ -32,34 +32,17 @@ function error() {
   echo -e "$COL_RED [error]$COL_RESET "$1
 }
 
-function reloadzsh() {
-  source zsh_files/.zshrc
-}
 
 bot "Hello! Let's get some stuff ready for your new computer"
 
 bot "Installing osx command line tools"
 xcode-select --install
 
-# install brew
 running "Installing brew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-reloadzsh
-
-running "Updating to most recent brew version"
-brew doctor
-brew update
-ok
-
-# running "Downloading oh-my-zsh"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# ok
-
 running "Downloading nvm"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh)"
-
-reloadzsh
 
 running "Installing the latest stable version of node"
 nvm install
@@ -67,14 +50,5 @@ nvm install
 running "Installing zx globally"
 npm i -g zx
 
-reloadzsh
-
 bot "Whooo, all set! "
 bot "Run zx ./setup.sh to setup your environment"
-
-# bot "Setting zsh as the user shell"
-# chsh -s /bin/zsh
-# ok
-
-# create a symblink for all zsh files
-ln -sf ~/dotfiles/zsh_files/.* ~/
